16ae28656c25d61f81bd120d86e8b6e1bec86a17
kanika <kanika@techsmarters.com>
2025-12-04 18:46:28 +0530

    live favourite

--- pages/LiveTvPage.js
+++ pages/LiveTvPage.js
@@ -1737,7 +1737,7 @@ return `
           <div class="epg-header">
             <img class="epg-channel-logo" src="/assets/channel.png" alt="Channel" />
             <button class="epg-favorite-btn">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke="none" stroke-width="2">
                 <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
               </svg>
             </button>
