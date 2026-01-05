c78bd708510b3ca4ce1263fbf9f7499504d1e99d
ranbirverma <ranbirverma.25@techsmaters.com>
2026-01-05 10:36:18 +0530

    values removed

--- api/apiFunctions.js
+++ api/apiFunctions.js
@@ -88,9 +88,9 @@ async function loginApi(
   // const defaultDns = "http://nubiatv.live/";z
     const defaultDns = "http://mega2025.site:8080/";
 
-  //  let alldns = JSON.parse(localStorage.getItem("all_dns")) || [];
+   let alldns = JSON.parse(localStorage.getItem("all_dns")) || [];
 
-  let alldns = [];
+  // let alldns = [];
 
 
   if (alldns.length === 0) {
